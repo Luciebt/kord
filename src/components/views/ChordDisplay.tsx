367ff1f4e32266d70b5d683a6d73e0b13d46e740
lbt <lbt@ableton.com>
2021-10-14 20:57:14 +0200

    WIP: Add Basic Key support to play chords of one progression

--- src/components/views/ChordDisplay.tsx
+++ src/components/views/ChordDisplay.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { PlayPianoChord } from "../../IChords";
 import PianoDisplay from "./PianoDisplay";
+import { useKeyPress } from "../hooks/KeyPressHook";
 
 export interface IChordDisplayProps {
   key?: number;
@@ -13,10 +14,37 @@ const ChordDisplayComponent = ({
 }: IChordDisplayProps): JSX.Element => {
   const [chordState, setChordState] = useState(false);
   const [chordSelected, setChordSelected] = useState("");
+  const chordArr: string[] = chord.split(",");
+
+  const FirstNumKey = useKeyPress("1");
+  const SecondNumKey = useKeyPress("2");
+  const ThirdNumKey = useKeyPress("3");
+  const FourthNumKey = useKeyPress("4");
+  const FifthNumKey = useKeyPress("5");
+  const SixthNumKey = useKeyPress("6");
 
   let chordButton: JSX.Element[] = [];
 
-  chord.split(",").forEach((c) => {
+  if (FirstNumKey) {
+    PlayPianoChord(chordArr[0]);
+  }
+  if (SecondNumKey) {
+    PlayPianoChord(chordArr[1]);
+  }
+  if (ThirdNumKey && chordArr.length > 2) {
+    PlayPianoChord(chordArr[2]);
+  }
+  if (FourthNumKey && chordArr.length > 3) {
+    PlayPianoChord(chordArr[3]);
+  }
+  if (FifthNumKey && chordArr.length > 4) {
+    PlayPianoChord(chordArr[4]);
+  }
+  if (SixthNumKey && chordArr.length > 5) {
+    PlayPianoChord(chordArr[5]);
+  }
+
+  chordArr.forEach((c) => {
     chordButton.push(
       <button
         key={c}
@@ -29,7 +57,6 @@ const ChordDisplayComponent = ({
     );
   });
 
-  // TODO: Play chords w keys 1, 2, 3, 4.
   return (
     <div className="">
       <h3>{chordButton}</h3>
