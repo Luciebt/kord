1e1047e63fac0339a4417f89e15e1fb14e508895
LBT <luciebtdev@gmail.com>
2021-11-04 22:02:12 +0100

    Updates

--- bundle.js
+++ bundle.js
@@ -83351,13 +83351,33 @@ exports.PlayPianoChords = PlayPianoChords;
 /*!****************************!*\
   !*** ./src/audio/Synth.ts ***!
   \****************************/
-/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
 
 "use strict";
 
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.PlayLoop = exports.PlaySynthChords = exports.disposeSynth = void 0;
 const tone_1 = __webpack_require__(/*! tone */ "./node_modules/tone/build/esm/index.js");
+const Tone = __importStar(__webpack_require__(/*! tone */ "./node_modules/tone/build/esm/index.js"));
 const PianoChart_1 = __webpack_require__(/*! ../PianoChart */ "./src/PianoChart.ts");
 let polySynth = new tone_1.PolySynth(tone_1.Synth, {
     volume: -8,
@@ -83390,6 +83410,7 @@ exports.disposeSynth = disposeSynth;
 function PlaySynthChords(chordNotes) {
     if (polySynth) {
         polySynth.triggerAttackRelease(chordNotes, "+0.05", 1);
+        Tone.start();
     }
 }
 exports.PlaySynthChords = PlaySynthChords;
