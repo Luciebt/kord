ffce9dcf72db31dd46c8fc037cde2c220c498a93
luciebt <luciebtdev@gmail.com>
2023-10-05 12:30:13 +0200

    Refactor Tab component

--- src/components/tabs/TabComponent.tsx
+++ src/components/tabs/TabComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import ProgressionDictionnaryTab from "../tabs/ProgressionDictionnaryTab";
 import ProgressionBuilderTab from "../tabs/ProgressionBuilderTab";
 import { unPressElementsStyleWithoutEvent } from "../../hooks/unPressElementStyle";
@@ -7,37 +7,36 @@ import "./Tab.css";
 const TabComponent = (): JSX.Element => {
   const [activeTab, setActiveTab] = useState("tab-id-1");
 
-  const handleTab = (event: any) => {
+  const handleTab = (event: React.MouseEvent<HTMLButtonElement>) => {
     unPressElementsStyleWithoutEvent("active-tab");
-    const tabId = event.target.id;
-    const tab = document.getElementById(tabId);
-    if (tab) tab.classList.add("active-tab");
-
+    event.currentTarget.classList.add("active-tab");
+    const tabId = event.currentTarget.id;
     setActiveTab("tab-" + tabId);
   };
 
-  useEffect(() => {
-    const tab1 = document.getElementById("id-1");
-    if (tab1) tab1.classList.add("active-tab");
-    return () => { };
-  }, []);
+  const renderTabContent = () => {
+    switch (activeTab) {
+      case "tab-id-1":
+        return <ProgressionDictionnaryTab />;
+      case "tab-id-2":
+        return <ProgressionBuilderTab />;
+      default:
+        return null;
+    }
+  };
 
   return (
     <div className="content">
       <ul arial-label="tabs" className="box tab-nav">
-        <button id="id-1" onClick={(e) => handleTab(e)}>
+        <button id="id-1" onClick={handleTab} className="active-tab">
           Progressions List
         </button>
-        <button id="id-2" onClick={(e) => handleTab(e)}>
+        <button id="id-2" onClick={handleTab}>
           Progressions Builder
         </button>
       </ul>
       <div className="outlet">
-        {activeTab === "tab-id-1" ? (
-          <ProgressionDictionnaryTab />
-        ) : (
-          <ProgressionBuilderTab />
-        )}
+        {renderTabContent()}
       </div>
     </div>
   );
