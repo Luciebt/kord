e230e834d06c9b7c21e70d9cdc74c96c84947841
luciebt <luciebtdev@gmail.com>
2025-03-19 22:00:16 +0100

    Cleanup Chords

--- src/Chords.ts
+++ src/Chords.ts
@@ -1,34 +1,24 @@
 import { Note, Chord } from "@tonaljs/tonal";
-import { detect } from "@tonaljs/chord-detect";
-import { PlaySynthChords, polySynth } from "./audio/Play";
-// import { SetupPiano, PlayPianoChords } from "./audio/Piano";
-import { CleanChords, SimplifiedChordNotationConverter } from "./utils/NoteUtils";
-
-// TODO: test accuracy of chords / their notes using chord-detect: https://github.com/tonaljs/tonal/tree/main/packages/chord-detect
+import { PlaySynthChords } from "./audio/Play";
+import {
+  CleanChords,
+  SimplifiedChordNotationConverter,
+} from "./utils/NoteUtils";
 
 enum InstrumentType {
   Piano,
   Synth,
 }
 
-// TODO: only setup piano if Piano instrument is chosen from the settings panel.
-// let PianoInSettings: boolean = false;
-
-// if (PianoInSettings) {
-//   SetupPiano();
-// }
-
 function PlayMidiNotes(
   chordNotes: string[],
-  instrumentType: InstrumentType
+  instrumentType: InstrumentType,
 ): void {
   switch (instrumentType) {
     case InstrumentType.Synth:
       PlaySynthChords(chordNotes);
       break;
     case InstrumentType.Piano:
-      const midiChordNotes: string[] = BuildMidiChordNotes(chordNotes);
-      // PlayPianoChords(midiChordNotes);
       break;
     default:
       break;
@@ -40,19 +30,9 @@ export function PlayChord(chord: string) {
   if (chordNotes) PlayMidiNotes(chordNotes, InstrumentType.Synth);
 }
 
-// To use only for grand piano sound.
-function BuildMidiChordNotes(chordNotes: string[]): string[] {
-  return chordNotes.map((note) => {
-    const NoteFound: any = Note.midi(note);
-    if (NoteFound) {
-      return NoteFound.toString();
-    }
-  });
-}
-
 export function GetSimplifiedChordFromFullChord(
   fullChord: string,
-  octave?: number
+  octave?: number,
 ): string[] {
   let [homeNote, chordMode] = SimplifiedChordNotationConverter(fullChord, true);
 
@@ -63,11 +43,11 @@ export function GetSimplifiedChordFromFullChord(
 export function BuildChordNotes(chord: string, octave: number = 3): string[] {
   const [chordMode, homeNote]: string[] = GetSimplifiedChordFromFullChord(
     chord,
-    octave
+    octave,
   );
   let chordNotesArr = Chord.getChord(chordMode, homeNote).notes;
 
   return chordNotesArr.map(
-    (chord) => Note.simplify(chord) && CleanChords(chord)
+    (chord) => Note.simplify(chord) && CleanChords(chord),
   );
 }
