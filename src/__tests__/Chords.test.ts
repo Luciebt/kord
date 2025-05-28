3e4decdb92c6923932d07a4ca4a49852222b1a92
luciebt <luciebtdev@gmail.com>
2025-05-28 21:31:11 +0200

    Add Chords tests

---
+++ src/__tests__/Chords.test.ts
@@ -0,0 +1,20 @@
+import { PlayChord, GetSimplifiedChordFromFullChord, BuildChordNotes } from '../Chords';
+import { PlaySynthChords } from "../audio/Play"; 
+
+// Mock the PlaySynthChords function
+jest.mock('../audio/Play', () => ({
+  PlaySynthChords: jest.fn(),
+}));
+
+describe('ChordPlayer', () => {
+
+  it('should return simplified chord notation correctly', () => {
+    const result = GetSimplifiedChordFromFullChord('C#m7');
+    expect(result).toEqual(['m7', 'C#']);
+  });
+
+  it('should return simplified chord notation with octave', () => {
+    const result = GetSimplifiedChordFromFullChord('Cmaj');
+    expect(result).toEqual(['maj', 'C']);
+  });
+});
